12a6f5d089d8c59e221bed7913477c1dbb1d7ead
Darkness Cat <45727717+TheDarknessCat@users.noreply.github.com>
2023-01-20 10:44:46 +0630

    remove prints

--- duplicate.py
+++ duplicate.py
@@ -1,3 +1,6 @@
+###
+move duplicate images to duplicate folder
+###
 from tkinter.filedialog import askdirectory
 from tkinter import Tk
 import os
@@ -5,10 +8,8 @@ import hashlib
 from pathlib import Path
 import shutil
 Tk().withdraw()
-# file_path = askdirectory(title="Select a folder")
 file_path = os.getcwd()
 path = os.getcwd()+"\duplicate"
-print(path)
 list_of_files = os.walk(file_path)
 unique_files = dict()
 os.makedirs(path)
@@ -19,10 +20,7 @@ for root, folders,files in list_of_files:
 		if Hash_file not in unique_files:
 			unique_files[Hash_file] = file_path
 		else:
-			print(f"{file_path} has been deleted")
-			# shutil.move(file_path,path , copy_function = path+str(file_path).split("test")[-1])
-			# shutil.move(file_path,path+str(file_path).split("twitter")[-1] , copy_function =shutil.copytree )
 			shutil.move(file_path,path, copy_function =shutil.copytree )
-			# os.remove(file_path)
+
    			
 			
